26a66545ef277eb92de7acb0492639e99a41d328
Marcos M Santos <marcos1970moreirasantos@gmail.com>
2022-07-16 00:14:06 -0300

    Security Config

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,8 +8,9 @@ function Header() {
                 < img src={logo} alt="DSMeta" />
                 <h1> DSMeta </h1>
                 <p>
-                    Desenvolvido por
+                    Desenvolvido por:
                     < a href=" https://www.instagram.com/devsuperior.ig " > @devsuperior.ig </ a >
+                    < a href=" https://www.linkedin.com/in/marcos-m-dos-santos-85071014a">/#MarcosSofia.in</a>
                 </ p >
             </ div >
             
