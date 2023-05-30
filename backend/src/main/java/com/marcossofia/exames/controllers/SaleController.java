4c1c069f7b5eaad24ddcba76e0772ffe6e287aa6
Marcos M Santos <marcos1970moreirasantos@gmail.com>
2023-05-30 12:11:03 -0300

    1_Rev_(Excl-Twille)

--- backend/src/main/java/com/marcossofia/exames/controllers/SaleController.java
+++ backend/src/main/java/com/marcossofia/exames/controllers/SaleController.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcossofia.exames.entities.Sale;
 import com.marcossofia.exames.services.SaleService;
-import com.marcossofia.exames.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -20,8 +18,7 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 
-	@Autowired
-	private SmsService smsService;
+	
 
 	@GetMapping
 	public Page<Sale> findSales(
@@ -32,10 +29,5 @@ public class SaleController {
 
 	}
 
-	@GetMapping("/{id}/notification")
-	public void notifySms(@PathVariable Long id) {
-		smsService.sendSms(id);
-
-	}
-
+	
 }
